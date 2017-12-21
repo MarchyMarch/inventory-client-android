a670b1ccb517e47efa2f9de59d11978e9d08b9ff
Kyle March <kcmarch23@gmail.com>
2017-12-20 19:21:51 -0700

    Initial commit with a blank activity

---
+++ app/src/main/java/edu/byu/cfaccsr/csr_inventory/ui/StartScreen.java
@@ -0,0 +1,17 @@
+package edu.byu.cfaccsr.csr_inventory.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.byu.cfaccsr.csr_inventory.R;
+
+public class StartScreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
